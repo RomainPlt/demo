090e827a248818b9d9d7258369cc9ca04a477800
romainplt <romain.pialat@imt-atlantique.net>
2021-01-05 14:35:35 +0100

    code cleansing

--- src/test/java/com/romainplt/secretVault/SecretVaultApplicationTests.java
+++ src/test/java/com/romainplt/secretVault/SecretVaultApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.romainplt.secretVault;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SecretVaultApplicationTests {
 
 	@Test
 	void contextLoads() {
