cec573dd2ac1e70485b72a0c639ea16a8d8006d2
romainplt <romain.pialat@imt-atlantique.net>
2021-01-05 17:35:54 +0100

    Link between Controller and Service establish

--- src/main/java/com/romainplt/secretvault/restservice/SecretService.java
+++ src/main/java/com/romainplt/secretvault/restservice/SecretService.java
@@ -2,24 +2,39 @@ package com.romainplt.secretvault.restservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
+@Service
 public class SecretService {
 
     public static Connection conn;
 
+    static {
+        try {
+            conn = DriverManager.getConnection("jdbc:h2:file:/tmp/secretVault/data/database", "sa", "admin");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
+
+    public SecretService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void createDatabase(){
+        new SecretService(jdbcTemplate);
+    }
     /*
     Will go through the H2 database to retrieve the Secret corresponding to the given Key.
     */
-    public static Secret getSecretFromDB(String key) throws SQLException {
+    public Secret getSecretFromDB(String key) throws SQLException {
         Secret secrets = null;
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS Secrets (key VARCHAR(255), secret VARCHAR(255));");
         System.out.println("Creating statement...");
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT secret FROM Secrets WHERE key = '" + key +"'");
@@ -29,8 +44,8 @@ public class SecretService {
         return secrets;
     }
 
-
-    public static void writeSecretToDB(JdbcTemplate jdbcTemplate, Secret secret) {
+    public void writeSecretToDB(Secret secret) {
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS Secrets (key VARCHAR(255), secret VARCHAR(255));");
         jdbcTemplate.execute("INSERT INTO Secrets(key,secret) VALUES ('" + secret.getKey() + "' ,'" + secret.getSecret() + "')");
     }
 
